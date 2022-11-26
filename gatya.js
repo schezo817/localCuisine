20ea4a7a11fa905dcb0bd3d6d7c0e88cf21aef0d
SrimpField <108197676+SrimpFeild@users.noreply.github.com>
2022-11-27 06:03:47 +0900

    編集

--- gatya.js
+++ gatya.js
@@ -15,22 +15,22 @@ async function load(){
 var obj = [{
   "kenmei": "大阪",
   "ryori": "たこ焼き",
-  "setumei": "美味しいよ"
+  "setumei": "美味しいよ食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたいv食べたい食べたい食べたい食べたい"
 },
 {
   "kenmei": "福岡",
   "ryori": "もつ鍋",
-  "setumei": "うまい"
+  "setumei": "うまい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい"
 },
 {
   "kenmei": "愛知",
   "ryori": "ひつまぶし",
-  "setumei": "食べたい"
+  "setumei": "食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい"
 },
 {
   "kenmei": "東京",
   "ryori": "深川丼",
-  "setumei": "匠の技"
+  "setumei": "匠の技食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい食べたい"
 }];
 
 
@@ -42,16 +42,28 @@ for(i in obj){
   ary.push(obj[i]);
 }
 
+function gatyaStart(){
+  //JSONデータの長さ分のランダム数
+  let rand = Math.floor(Math.random()*ryorilength);
+  //key設定
+  let key = Object.keys(obj);
+  let key1 = key[rand];
+  
+  document.getElementById('result1').textContent = '県名'+"："+ ary[rand].kenmei;
+  document.getElementById('result2').textContent = '料理名'+"："+ary[rand].ryori;
+  document.getElementById('result3').textContent = '説明'+"："+ary[rand].setumei;
+}
+/*
 //JSONデータの長さ分のランダム数
 let rand = Math.floor(Math.random()*ryorilength);
 //key設定
 let key = Object.keys(obj);
 let key1 = key[rand];
 
-document.getElementById('result').innerHTML = '<p style="text-align: center" class="ryorimei">県名'+"："+ ary[rand].kenmei+'</p>';
-document.getElementById('result').innerHTML += '<br><p style="text-align: center">料理名'+"："+ary[rand].ryori+ '</b></p>';
-document.getElementById('result').innerHTML += '<br><p style="text-align: center">説明'+"："+ary[rand].setumei+ '</b></p>';
-
+document.getElementById('result1').textContent = '県名'+"："+ ary[rand].kenmei;
+document.getElementById('result2').textContent = '料理名'+"："+ary[rand].ryori;
+document.getElementById('result3').textContent = '説明'+"："+ary[rand].setumei;
+*/
 
 
 
