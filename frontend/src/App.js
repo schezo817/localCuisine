a256af6553015c1f1a73940207d1f32067b8ce2a
schezo817 <akito4440@gmail.com>
2022-11-26 16:10:21 +0900

    要素のみ実装

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GachaScreen from "./GachaScreen";
+import HomeScreen from "./HomeScreen";
+import SearchScreen from "./SearchScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/GachaScreen" element={<GachaScreen />} />
+        <Route path="/SearchScreen" element={<SearchScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
